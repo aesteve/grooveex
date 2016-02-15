18a4d20037ff42b63091036ff3babdc0e25bf331
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-02-15 22:24:05 +0100

    0.8 : CorsHandler

---
+++ src/main/groovy/com/github/aesteve/vertx/groovy/CorsHandlerExtension.groovy
@@ -0,0 +1,15 @@
+package com.github.aesteve.vertx.groovy
+
+import groovy.transform.TypeChecked
+import io.vertx.core.http.HttpMethod
+import io.vertx.groovy.ext.web.handler.CorsHandler
+
+@TypeChecked
+class CorsHandlerExtension {
+
+	static CorsHandler allowedMethods(CorsHandler self, HttpMethod... methods) {
+		methods?.each { self.allowedMethod it }
+		self
+	}
+
+}
