565b524048f1f1be74bc1e2eec3cdb7c0e86ad2f
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-02-15 21:35:10 +0100

    bump to vertx 3.2.1 + remove behaviour driven testing (not good atm)

--- src/test/groovy/com/github/aesteve/vertx/groovy/specs/test/AsyncSpec.groovy
+++ src/test/groovy/com/github/aesteve/vertx/groovy/specs/test/AsyncSpec.groovy
@@ -4,11 +4,13 @@ import com.github.aesteve.vertx.groovy.annot.BehaviourDriven
 import com.github.aesteve.vertx.groovy.builder.RouterBuilder
 import com.github.aesteve.vertx.groovy.specs.TestBase
 import io.vertx.groovy.ext.unit.TestContext
+import org.junit.Ignore
 import org.junit.Test
 
 class AsyncSpec extends TestBase {
 
 	@Test
+	@Ignore
 	@BehaviourDriven
 	void test(TestContext context) {
 		when { client.get('/api/test') }
