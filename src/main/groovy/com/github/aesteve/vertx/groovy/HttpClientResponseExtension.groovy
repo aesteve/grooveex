33d59b7f640d646dfda9efa66659a058b8aa053a
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-25 21:14:14 +0200

    Add Closure support w/ delegate

--- src/main/groovy/com/github/aesteve/vertx/groovy/HttpClientResponseExtension.groovy
+++ src/main/groovy/com/github/aesteve/vertx/groovy/HttpClientResponseExtension.groovy
@@ -3,12 +3,21 @@ package com.github.aesteve.vertx.groovy
 import groovy.transform.TypeChecked
 import io.vertx.core.Handler
 import io.vertx.groovy.core.MultiMap
+import io.vertx.groovy.core.buffer.Buffer
 import io.vertx.groovy.core.http.HttpClientResponse
 
 @TypeChecked
 class HttpClientResponseExtension {
-	static void rightShiftUnsigned(HttpClientResponse self, Handler handler) {
+
+	static void rightShiftUnsigned(HttpClientResponse self, Handler<Buffer> handler) {
 		self.bodyHandler handler
+	}
+
+	static void rightShiftUnsigned(HttpClientResponse self, Closure<?> handler) {
+		self.bodyHandler { buff ->
+			handler.delegate = buff
+			handler buff
+		}
 	}
 
 	static MultiMap getHeaders(HttpClientResponse self) {
