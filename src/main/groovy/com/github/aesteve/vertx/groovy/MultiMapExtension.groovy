8f108d529943e06474f94f1da220feaf45dcb4b2
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-25 21:15:59 +0200

    TypeChecked

--- src/main/groovy/com/github/aesteve/vertx/groovy/MultiMapExtension.groovy
+++ src/main/groovy/com/github/aesteve/vertx/groovy/MultiMapExtension.groovy
@@ -1,14 +1,16 @@
 package com.github.aesteve.vertx.groovy
 
+import groovy.transform.TypeChecked
 import io.vertx.groovy.core.MultiMap
 
+@TypeChecked
 class MultiMapExtension {
 
 	static Object getAt(MultiMap self, CharSequence key) {
 		self.get key.toString()
 	}
 
-	static void putAt(MultiMap self, CharSequence key, Object value) {
+	static void putAt(MultiMap self, CharSequence key, String value) {
 		self.add key.toString(), value
 	}
 
@@ -19,12 +21,11 @@ class MultiMapExtension {
 	}
 
 	static MultiMap minus(MultiMap self, CharSequence toRemove) {
-		self.remove toRemove
+		self.remove toRemove.toString()
 	}
 
 	static MultiMap minus(MultiMap self, String toRemove) {
 		self.remove toRemove
 	}
-
 
 }
